dc01b88dfdc477579ec60a2b0340cae114acfada
pcbill <pcbill@gmail.com>
2014-04-02 12:30:24 +0800

    Changes to be committed: 	deleted:    src/main/java/wula/Main.java 	new file:   src/main/java/wula/Sample.java 	modified:   src/test/java/wula/HttpServerTest.java 	modified:   web/webapps/context/WEB-INF/spring-servlet.xml
    update sample content

---
+++ src/main/java/wula/Sample.java
@@ -0,0 +1,13 @@
+package wula;
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		/**
+		 * Create a web server, and start.
+		 */
+//		new HttpServer(8080, "webApp").startUp();
+	}
+	
+}
--- src/test/java/wula/HttpServerTest.java
+++ src/test/java/wula/HttpServerTest.java
@@ -1,9 +1,5 @@
 package wula;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
